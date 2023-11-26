ca17a0c18268f29267f7cfaf9b25054156248078
ZaytsNick <10kcin01@gmail.com>
2023-11-26 18:34:32 +0500

    task#1 remake

--- task#1/main1.cpp
+++ task#1/main1.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-
+void my_swap(int *pointer_a, int *pointer_b)
+{
+    int temp = *pointer_a;
+    *pointer_a = *pointer_b;
+    *pointer_b = temp;
+}
 int main()
 {
     int a = 10;
     int *pa = &a;
     int b = 20;
     int *pb = &b;
-    std::swap(*pa, *pb);
-
+    std::cout << a << " " << b<<std::endl;
+    my_swap(pa, pb);
     std::cout << a << " " << b;
-
-    // 20 10
 }
